bdeca66ae67d57b78aef266da5f5e68738e34847
LindseyNastos <lindseynastos@gmail.com>
2015-07-14 17:11:02 -0700

    starting personal project

---
+++ Pinterest/Pinterest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Pinterest.Startup))]
+namespace Pinterest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
